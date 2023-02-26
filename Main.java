878cd24e9cef9f54e09832d544ef086aa354f69d
shahwaizse <mshahwaiz12@gmail.com>
2023-02-26 07:25:38 +0500

    added face detection logic

--- Main.java
+++ Main.java
@@ -1,17 +1,28 @@
-import org.opencv.core.Mat;
+import org.opencv.core.*;
+import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.videoio.VideoCapture;
 import static org.opencv.highgui.HighGui.*;
-import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
 
 public class Main {
     public static void main(String[] args){
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME); //code won't compile without this line.
         Mat frame = new Mat();
         VideoCapture capture = new VideoCapture(0);
+        CascadeClassifier face = new CascadeClassifier("C:\\Users\\mshah\\IntelliJ Projects\\java-cv\\.cascade\\face-cascade.xml");
+        MatOfRect coordinates = new MatOfRect();
         while(true){
             capture.read(frame);
-            imshow("oogabooga", frame);
-            int keyPress = waitKey(1);
+            face.detectMultiScale(frame, coordinates, 1.1, 5);
+            if(!coordinates.empty()) {
+                Imgproc.rectangle(frame, coordinates.toList().get(0), new Scalar(255, 0, 0), 1, 8, 0);
+                System.out.println("face detected");
+            }
+            else{
+                System.out.println("searching for faces");
+            }
+            imshow("frame", frame);
+            int keyPress = waitKey(10);
             if(keyPress > 0){
                 System.exit(0);
             }
